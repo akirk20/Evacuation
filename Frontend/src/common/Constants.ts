5a5e9f3d2e5750f76a2a59306d9ee1ac32f88f41
Anne Cathrine <akirk20@student.sdu.dk>
2022-12-21 03:51:26 +0100

    changed url

--- Frontend/src/common/Constants.ts
+++ Frontend/src/common/Constants.ts
@@ -1,5 +1,5 @@
 const constansts = {
-    serverBaseURL: 'http://35.205.83.138:3000' // external ip på CLUSTERET
+    serverBaseURL: 'http://35.205.61.155:3000' // external ip på CLUSTERET
 }
 
 export default constansts;
